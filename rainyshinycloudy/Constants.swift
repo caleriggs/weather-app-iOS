1189d17bc0a3a7715c9424bd33e030ba65a3af68
Cale Riggs <caleriggs@Cales-MacBook-Pro.local>
2017-03-27 14:01:24 -0500

    url update

--- rainyshinycloudy/Constants.swift
+++ rainyshinycloudy/Constants.swift
@@ -10,13 +10,14 @@ import Foundation
 
 
 let BASE_URL = "api.openweathermap.org/data/2.5/weather?"
-let ZIP_CODE = "zip=63146,us"
+let LATITUDE = "lat=38.62"
+let LONGITUDE = "&lon=-90.19"
 let APP_ID = "&APPID="
 let API_KEY = "ca94eb1029ee482d2dc7486f0d50d2e5"
 
 typealias DownloadComplete = () -> ()
 
-let CURRENT_WEATHER_URL = "\(BASE_URL)\(ZIP_CODE)\(APP_ID)\(API_KEY)"
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)\(LONGITUDE)\(APP_ID)\(API_KEY)"
 
 /*
  OPEN WEATHER LINK REFERENCES (DELETE LATER)
